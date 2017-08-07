3e1f686bab05fab976cddca3aae31612b99ecafa
Hannu Saransaari <hannu.saransaari@gmail.com>
2017-08-07 15:59:08 +0300

    Setup Vundle and YCM

--- setup.sh
+++ setup.sh
@@ -3,6 +3,18 @@
 set -e -x
 
 mkdir -p ~/tmp
+mkdir -p ~/.vim/bundle
+
+rm -fr ~/.vim/bundle/Vundle.vim
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo "source `pwd`/vimrc" > ~/.vimrc
+
+vim +PluginInstall +qall
+
+pushd .
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer
+popd
 
